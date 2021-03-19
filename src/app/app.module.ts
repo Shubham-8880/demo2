ba041a9e421cce1248d9932080d61977d6e16e44
Shubham-8880 <shubham888jadhav@gmail.com>
2021-03-19 13:01:14 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { AsideComponent } from './aside/aside.component';
+import { ContentComponent } from './content/content.component';
+import { ArticleComponent } from './article/article.component';
+import { SectionComponent } from './section/section.component';
+import { Section1Component } from './section1/section1.component';
+import { Section2Component } from './section2/section2.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    NavigationComponent,
+    AsideComponent,
+    ContentComponent,
+    ArticleComponent,
+    SectionComponent,
+    Section1Component,
+    Section2Component,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
